75d2a8a250446a5c3f98b0b55befa7a395bf60a2
aayushchauhan10 <ai.aayush10@gmail.com>
2023-08-28 19:07:08 +0530

    Added 6th Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="headerBg h-[50vh] xl:h-[76vh] xxxl:h-[80vh] xl5:h-[85vh] xl7:h-[87vh] ">
-      <h1 className="flex justify-center font-bold text-white text-[32px] lg:text-[52px] font-nunito items-center h-[50vh] xl:h-[76vh] xxxl:h-[80vh] xl5:h-[85vh] xl7:h-[87vh]   ">
+    <div className="headerBg h-[46.2vh] xl:h-[70vh] xxxl:h-[80vh] xl5:h-[85vh] xl7:h-[87vh] ">
+      <h1 className="flex justify-center font-bold text-white text-[36px] lg:text-[52px] font-nunito items-center h-[50vh] xl:h-[76vh] xxxl:h-[80vh] xl5:h-[85vh] xl7:h-[87vh]   ">
         Our Pricing
       </h1>
     </div>
